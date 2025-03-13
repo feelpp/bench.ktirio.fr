102cdfa37d0ab97afad87fed0519fe31a6b2c756
Javier Cladellas <javiercladellas@gmail.com>
2025-03-13 15:03:12 +0100

    add deployment ids

---
+++ girder_deploy_config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+production_website_config_id=67d2e590da2d67521b36adcd
+staging_folder_id=67d2cbe1da2d67521b36adca
+production_folder_id=67d2cbc3da2d67521b36adc8
+denied_folder_id=67d2cbc7da2d67521b36adc9
