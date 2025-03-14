3c8c1bf4bdf8e6afd181712ec6d2d0b2d3b0e085
Javier Cladellas <javiercladellas@gmail.com>
2025-03-14 11:42:04 +0100

    add accepted folder id [ci skip]

--- girder_deploy_config.sh
+++ girder_deploy_config.sh
@@ -4,3 +4,4 @@ production_website_config_id=67d2e590da2d67521b36adcd
 staging_folder_id=67d2cbe1da2d67521b36adca
 production_folder_id=67d2cbc3da2d67521b36adc8
 denied_folder_id=67d2cbc7da2d67521b36adc9
+accepted_folder_id=67d407a2da2d67521b36ae2e
