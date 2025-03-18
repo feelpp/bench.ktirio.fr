78aed415c6c79335a42d2f217cb368d7594b47b9
Javier Cladellas <javiercladellas@gmail.com>
2025-03-18 11:38:00 +0100

    reverse

--- tmp_system_entrypoints/vega.sh
+++ tmp_system_entrypoints/vega.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-source /ceph/hpc/software/cvmfs_env.sh
 module use /usr/share/Modules/modulefiles
+source /ceph/hpc/software/cvmfs_env.sh
 
 module load Python/3.10.8-GCCcore-12.2.0
 python3 -m venv .venv
