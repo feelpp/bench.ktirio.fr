99ba1e0598b18e38402cb679e636cf4959041987
Javier Cladellas <javiercladellas@gmail.com>
2025-03-18 11:39:51 +0100

    source bashrc [ci skip]

--- tmp_system_entrypoints/vega.sh
+++ tmp_system_entrypoints/vega.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-module use /usr/share/Modules/modulefiles
-source /ceph/hpc/software/cvmfs_env.sh
+
+source $HOME/.bashrc
 
 module load Python/3.10.8-GCCcore-12.2.0
 python3 -m venv .venv
